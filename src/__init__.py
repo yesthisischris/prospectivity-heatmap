31999e27f613809e9e6b92884fb8826a2ecdd0ab
yesthisischris <87864614+yesthisischris@users.noreply.github.com>
2025-07-19 22:10:31 -0700

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""Package initialisation for prospectivity heatmap.
+
+This package exposes configuration and pipeline functions for building a
+geospatial prospectivity heatmap using H3 indexing. See individual modules
+for details.
+"""
